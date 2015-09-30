dbd0e79dbc64fc70e483288bb4594870cc8fa6ad
wangfulin <1622913826@qq.com>
2015-09-30 10:53:54 +0800

    pre exercise

--- NodeJs/microblog/app.js
+++ NodeJs/microblog/app.js
@@ -8,11 +8,14 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var partials=require("express-partials");
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,8 +24,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
+//app.use("/hello",routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -56,5 +59,14 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.get("/list",function(req,res){
+	res.render("list",{
+		title:"List",
+		items:[1992,"wangfulin","express","nodejs"]
+	});
+});
+
+//route starts here
+//app.get("/hello",routes.hello);
 
 module.exports = app;
--- NodeJs/microblog/routes/index.js
+++ NodeJs/microblog/routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Display current time */
+router.get("/hello",function(req,res,next){
+	res.send("The time is "+new Date().toString());
+});
+
+/* multi-method */
+router.all("/user/:username",function(req,res,next){
+	console.log("all methods captured");
+	next();
+});
+
+/* Show personal website */
+router.get("/user/:username",function(req,res,next){
+	res.send("user:"+req.params.username);
+});
+
+/* Use regexp */
+router.get(new RegExp("\/user\/([^\/]+)\/([^\/]+)\/?"),function(req,res,next){
+	res.send("params1:"+req.params[0]+" params2:"+req.params[1]);
+});
+
+/* List items */
+router.get("/list",function(req,res,next){
+	res.render("list",{
+		title:"List",
+		items:[1992,"wangfulin","express","nodejs"]
+	});
+});
+
 module.exports = router;
